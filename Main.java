820f45097bde002e73fbbaf3a730ec5db9b1f8a5
koirii <99716645+koirii@users.noreply.github.com>
2022-02-21 17:15:28 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package org.zerock;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	
         BMIService bmiService =new BMIService();
         BMIUI bmiui=new BMIUI(bmiService);
 
